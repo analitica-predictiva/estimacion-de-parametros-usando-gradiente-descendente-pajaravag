14d6fe39f5e45ce2074e389d22276c0c2b008cf4
Pablo Andres <codechemfin@gmail.com>
2022-10-31 08:12:03 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -19,7 +19,7 @@ def pregunta_01():
     from sklearn.preprocessing import PolynomialFeatures
 
     # Cargue el dataset `data.csv`
-    data = pd.read_csv("data")
+    data = pd.read_csv("data.csv")
 
     # Cree un objeto de tipo `PolynomialFeatures` con grado `2`
     #poly = ___.___(___)
